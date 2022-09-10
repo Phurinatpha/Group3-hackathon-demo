c0385602e6ea78b6b9c169446bdf696d5f085e0c
Phurinatpha <phurinat_phanuphong@cmu.ac.th>
2022-09-10 16:20:47 +0700

    commit barcode->sku->product detail

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -10,13 +10,11 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.demo.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.firebase.firestore.util.Assert
 import com.google.gson.Gson
 import okhttp3.*
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.chromium.base.Promise
 import java.io.IOException
-import java.io.StringBufferInputStream
-import java.util.*
-import kotlin.reflect.typeOf
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,7 +26,10 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        testapi()
+
+        testapigetsku("2502009951898", ::getSkuSuccess)
+
+
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
@@ -51,7 +52,43 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun testapi(){
+    private fun getSkuSuccess(sku: String) {
+        println("here get sku =" + sku)
+        testapi(sku)
+    }
+
+    private fun testpostapi(){
+        val payload = "test payload"
+        println("tesssssssstttttttttttt")
+        val okHttpClient = OkHttpClient()
+        val requestBody = payload.toRequestBody()
+        val request = Request.Builder()
+            .method("POST", requestBody)
+            .url("https://data.mongodb-api.com/app/data-swrdf/endpoint/data/v1/action/findOne")
+            .addHeader("Content-Type", "application/json")
+            .addHeader("Access-Control-Request-Headers", "*")
+            .addHeader("api-key", "ileFMdqL4lKjPcWHlWMLjkHlB7vCqGN0hfS8ZWqqGyRUN1VYPr3ILzQqjzaWVUu5")
+            .build()
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                print("not in faillerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr")
+            }
+            override fun onResponse(call: Call, response: Response) {
+                val gson = Gson()
+                val tex = response.body?.string()
+                //val model = gson.fromJson(tex, Model::class.java)
+                if (tex != null) {
+                    println("posttttttttttttttttttttttttttttttttttttt")
+                    println(tex)
+                }else {
+                    println(tex)
+                }
+                println("posttttttttttttttttttttttttttttttttttttt")
+            }
+        })
+    }
+
+    private fun testapi(sku : String ){
         data class  status(
             val code : Int,
             val message : String
@@ -60,6 +97,7 @@ class MainActivity : AppCompatActivity() {
         data class list_img(
             val url: String
         )
+
         data class regularPrice(
             val value :String,
             val currency : String
@@ -100,36 +138,79 @@ class MainActivity : AppCompatActivity() {
 
         )
 
-
         data class TestModel(
             val status: status,
             val data: Data
         )
-
+        var url_data = "https://ppe-api.lotuss.com/proc/product/api/v1/products/details?websiteCode=thailand_hy&storeId=5016&sku="+sku
         val request = Request.Builder()
-            //.url("https://ppe-api.lotuss.com/proc/product/api/v1/products/details?websiteCode=thailand_hy&storeId=5016&sku=51202528%22")
-            .url("https://ppe-api.lotuss.com/proc/product/api/v1/products/details?websiteCode=thailand_hy&storeId=5016&sku=51202528")
+            .url(url_data)
             .addHeader("accept-language", "th")
             .addHeader("Authorization", "Basic ZWY4ZTZjMjgzODdlNGVjYTlkM2UxMTU1MDQxMjgyYzE6MEU1NTg0QzUxZTdBNDBEODkzMDUxZGExY2NEQTg2ZTY=")
             .build()
         client.newCall(request).enqueue(object : Callback{
             override fun onFailure(call: Call, e: IOException) {}
             override fun onResponse(call: Call, response: Response){
-                var gson = Gson()
-                var tex = response.body?.string()
-                var testModel = gson.fromJson(tex, TestModel::class.java)
+                val gson = Gson()
+                val tex = response.body?.string()
+                val testModel = gson.fromJson(tex, TestModel::class.java)
                 if (testModel != null) {
-                    println("hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
-
                     println(testModel)
                 }else {
                     println(tex)
                 }
-                println("hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
-
             }
 
         })
     }
+
+//    private fun testapigetcustomer(){
+//        var str_pnum = "66810000001"
+//        var url_phone = "https://script.google.com/macros/s/AKfycbwBJ4REN3fhSb1_KSDMATnGXUHzHMmt9Wh-xHFViLqhzk4Lac3xDLlnZKtPBK1lTzJd/exec?topic=customer&data="+str_pnum
+//        val request = Request.Builder()
+//            .url(url_phone)
+//            .build()
+//        client.newCall(request).enqueue(object : Callback{
+//            override fun onFailure(call: Call, e: IOException) {}
+//            override fun onResponse(call: Call, response: Response){
+//                val tex = response.body?.string()
+////                val gson = Gson()
+////
+////                val testModel = gson.fromJson(tex, TestModel::class.java)
+//                if (tex != null) {
+//                    println(tex)
+//                }else {
+//                }
+//            }
+//
+//        })
+//    }
+
+    private fun testapigetsku(str_bar: String, callback: (String) -> Unit) {
+        data class Model(
+            val barcode: String,
+            val sku : String
+        )
+        var sku_u : String
+        var url_bar = "https://script.google.com/macros/s/AKfycbwBJ4REN3fhSb1_KSDMATnGXUHzHMmt9Wh-xHFViLqhzk4Lac3xDLlnZKtPBK1lTzJd/exec?topic=barcode&data="+str_bar
+        val request = Request.Builder()
+            .url(url_bar)
+            .build()
+        client.newCall(request).enqueue(object : Callback{
+            override fun onFailure(call: Call, e: IOException) {}
+            override fun onResponse(call: Call, response: Response)  {
+                val tex = response.body?.string()
+                val gson = Gson()
+                val model = gson.fromJson(tex, Model::class.java)
+                if (model != null) {
+                    //println(model)
+                    sku_u = model.sku
+                    callback(sku_u)
+                }
+            }
+
+        })
+    }
+
 }
 
